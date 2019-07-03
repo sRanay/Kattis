f72cf670627a66108d5aee5788256df0cd7fa314
sRanay <jsong2506@gmail.com>
2019-07-03 15:15:37 +0800

    Added 3 new problems

--- Mjehuric/mjehuric.cpp
+++ Mjehuric/mjehuric.cpp
@@ -11,29 +11,24 @@ int main() {
 	while(true) {
 		if(numlist[0] > numlist[1]) {
 			swap(numlist[0],numlist[1]);
-			for(int i = 0;i < 5;i++) {
-				cout << numlist[i] << ' ';
-			}
-			cout << endl;
 		} else if(numlist[1] > numlist[2]) {
 			swap(numlist[1],numlist[2]);
-			for(int i = 0;i < 5;i++) {
-				cout << numlist[i] << ' ';
-			}
-			cout << endl;
 		} else if(numlist[2] > numlist[3]) {
 			swap(numlist[2],numlist[3]);
-			for(int i = 0;i < 5;i++) {
-				cout << numlist[i] << ' ';
-			}
-			cout << endl;
 		} else if(numlist[3] > numlist[4]) {
 			swap(numlist[3],numlist[4]);
-			for(int i = 0;i < 5;i++) {
-				cout << numlist[i] << ' ';
-			}
-			cout << endl;
 		} else {
+			break;
+		}
+		for(int i = 0;i < 5;i++) {
+			cout << numlist[i] << ' ';
+		}
+		cout << "\n";
+		if(numlist[0] == 1 && 
+			numlist[1] == 2 &&
+			numlist[2] == 3 &&
+			numlist[3] == 4 &&
+			numlist[4] == 5) {
 			break;
 		}
 	}
